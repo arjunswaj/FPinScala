4c92f5576ce1b1fd4608d9a057f1615744e52e5b
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-10-29 14:22:55 +0530

    Random Pair

--- Chapter 6/src/com/asb/rng/RNG.scala
+++ Chapter 6/src/com/asb/rng/RNG.scala
@@ -9,10 +9,28 @@ trait RNG {
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
+
   def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
   }
+
+}
+
+object random {
+
+  def sameRandomPair(rng: RNG): (Int, Int) = {
+    val (i1, _) = rng.nextInt
+    val (i2, _) = rng.nextInt
+    (i1, i2)
+  }
+
+  def randomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
+  }
+
 }
--- Chapter 6/src/com/asb/rng/StatefulWS.sc
+++ Chapter 6/src/com/asb/rng/StatefulWS.sc
@@ -1,5 +1,8 @@
-import com.asb.rng.SimpleRNG
+import com.asb.rng.{SimpleRNG, random}
 
 val rng = SimpleRNG(42)
 val (n1, rng2) = rng.nextInt
 val (n2, rng3) = rng2.nextInt
+
+val (s1, s2) = random.sameRandomPair(rng)
+val ((m1, m2), rng4) = random.randomPair(rng)
