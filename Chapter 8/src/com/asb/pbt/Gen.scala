0f086652807ab6199d6f8c680049c642ed6b5a3d
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-12-04 15:05:03 +0530

    forAll

--- Chapter 8/src/com/asb/pbt/Gen.scala
+++ Chapter 8/src/com/asb/pbt/Gen.scala
@@ -6,11 +6,16 @@ package com.asb.pbt
   */
 case class Gen[A]()
 
+trait Prop {
+  def &&(p: Prop): Prop
+}
 
 object Gen {
 
   def listOf[A](a: Gen[A]): Gen[List[A]] = ???
 
   def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = ???
+
+  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
 
 }
