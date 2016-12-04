578bcf9d60ca606d4bc11fc100bbb524aec5335f
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-12-04 14:59:09 +0530

    Gen - listOf, listOfN signature

---
+++ Chapter 8/src/com/asb/pbt/Gen.scala
@@ -0,0 +1,16 @@
+package com.asb.pbt
+
+/**
+  * Gen
+  * Created by arjun on 04/12/16.
+  */
+case class Gen[A]()
+
+
+object Gen {
+
+  def listOf[A](a: Gen[A]): Gen[List[A]] = ???
+
+  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = ???
+
+}
