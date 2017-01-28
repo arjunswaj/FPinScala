fb12dcaaabff05af0aaa866263dddf46fb69d00c
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-01-28 22:46:59 +0530

    String Parser

--- Chapter 9/src/com/asb/parsers/Parsers.scala
+++ Chapter 9/src/com/asb/parsers/Parsers.scala
@@ -10,4 +10,6 @@ trait Parsers[ParseError, Parser[+ _]] {
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   def char(c: Char): Parser[Char]
+
+  def string(s: String): Parser[String]
 }
