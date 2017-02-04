209ae273b1c4e125ab67e51d5bcd51a4e870cf04
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-02-04 13:13:35 +0530

    Or and Implicits

--- Chapter 9/src/com/asb/parsers/Parsers.scala
+++ Chapter 9/src/com/asb/parsers/Parsers.scala
@@ -7,9 +7,23 @@ import com.asb.error.Either
   * Created by arjun on 28/01/17.
   */
 trait Parsers[ParseError, Parser[+ _]] {
+  self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   def char(c: Char): Parser[Char]
 
-  def string(s: String): Parser[String]
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+  }
+
 }
