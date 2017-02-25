3c20edebd9b8a6f4e88c3ff136eb470ddb8e5f0c
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-02-25 19:53:44 +0530

    Few monoidInstances

--- Monoids/src/com/asb/monoids/MonoidInstances.scala
+++ Monoids/src/com/asb/monoids/MonoidInstances.scala
@@ -17,4 +17,27 @@ object MonoidInstances {
     override def zero = Nil
   }
 
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+
+    override def zero = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero = true
+  }
 }
