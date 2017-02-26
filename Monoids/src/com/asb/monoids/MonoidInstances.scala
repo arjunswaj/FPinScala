b095dcfefd66e1bb02532f7da2a1bfa6261343d6
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-02-26 19:38:31 +0530

    foldMap

--- Monoids/src/com/asb/monoids/MonoidInstances.scala
+++ Monoids/src/com/asb/monoids/MonoidInstances.scala
@@ -70,4 +70,7 @@ object MonoidInstances {
   def concatenate[A](as: List[A], m: Monoid[A]): A =
     as.foldLeft(m.zero)(m.op)
 
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
+    as.map(f(_)).foldLeft(m.zero)(m.op)
+
 }
