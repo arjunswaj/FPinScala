3d5d23a3c6cb6449699294916f264973633f0d6d
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-04-01 17:54:56 +0530

    functionMonoid

--- Monoids/src/com/asb/monoids/MonoidInstances.scala
+++ Monoids/src/com/asb/monoids/MonoidInstances.scala
@@ -121,4 +121,10 @@ object MonoidInstances {
       def zero: (A, B) = (a.zero, b.zero)
     }
 
+  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
+    def op(f: A => B, g: A => B): A => B = a => B.op(f(a), g(a))
+
+    def zero: A => B = a => B.zero
+  }
+
 }
