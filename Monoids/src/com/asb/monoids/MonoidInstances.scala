0197ea7c79d416be9289de82c4b9cdbc7e047810
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-04-01 18:33:47 +0530

    mapMergeMonoid

--- Monoids/src/com/asb/monoids/MonoidInstances.scala
+++ Monoids/src/com/asb/monoids/MonoidInstances.scala
@@ -127,4 +127,12 @@ object MonoidInstances {
     def zero: A => B = a => B.zero
   }
 
+  def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] = new Monoid[Map[K, V]] {
+    def op(a: Map[K, V], b: Map[K, V]): Map[K, V] =
+      (a.keySet ++ b.keySet).foldLeft(zero)((acc, k) =>
+        acc.updated(k, V.op(a.getOrElse(k, V.zero), b.getOrElse(k, V.zero))))
+
+    def zero: Map[K, V] = Map[K, V]()
+  }
+
 }
