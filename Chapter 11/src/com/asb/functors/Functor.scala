d34d42ebe55c2923402ced838f127493feef880b
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-04-08 12:18:33 +0530

    codistribute

--- Chapter 11/src/com/asb/functors/Functor.scala
+++ Chapter 11/src/com/asb/functors/Functor.scala
@@ -7,4 +7,9 @@ trait Functor[F[_]] {
   def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
     (map(fab)(_._1), map(fab)(_._2))
 
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(a) => map(a)(Left(_))
+    case Right(b) => map(b)(Right(_))
+  }
+
 }
