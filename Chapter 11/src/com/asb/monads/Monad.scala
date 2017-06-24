4eac65f205a9a617b2f426e7fd3557d56c389938
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-06-24 11:30:40 +0530

    Id Monad

--- Chapter 11/src/com/asb/monads/Monad.scala
+++ Chapter 11/src/com/asb/monads/Monad.scala
@@ -60,3 +60,11 @@ case class Id[A](value: A) {
 
   def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
+
+object Id {
+  val idMonad = new Monad[Id] {
+    def unit[A](a: => A): Id[A] = Id(a)
+
+    def flatMap[A, B](fa: Id[A])(f: (A) => Id[B]): Id[B] = fa flatMap f
+  }
+}
